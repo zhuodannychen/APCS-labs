91434fbcc82466df2cd1ebccf7f718bc8f96995b
Danny Chen <37634165+zhuodannychen@users.noreply.github.com>
2018-11-15 12:56:32 -0600

    Add files via upload

---
+++ FirstLab.java
@@ -0,0 +1,59 @@
+/** 
+ * The FirstLab class <My program prints my name with my house address with a frame. For the bonus points, my program prints a silly man with a hat>
+ *
+ * @author <Zhuo> <Chen>
+ * Collaborators: <None>
+ * Teacher Name: Mrs.Garj
+ * Period: 3rd
+ * Due Date: 8-22-18
+ */
+
+public class FirstLab 
+{
+	public static void main(String[] args) 
+	{
+		// This code prints my name with a frame
+		
+		System.out.println("<*******************>");
+		System.out.println("|    \"Zhuo Chen\"    |");
+		System.out.println("<*******************>");
+		System.out.println("|  4017 Bashful Dr. |");
+		System.out.println("<*******************>");
+		System.out.println("|  Plano, TX 75093  |");
+		System.out.println("<*******************>");
+		
+
+		
+		
+		
+		
+		
+		// This calls the bonus method to draw your silly face/picture
+		bonus();
+	}
+	
+	/** Draws a silly face or picture
+	 */	
+	public static void bonus()
+	{
+		// This prints a silly man with his tong out
+		
+		System.out.println();
+		System.out.println("+---------------------+");
+		System.out.println("|                     |");
+		System.out.println("|      ^   *   ^      |");
+		System.out.println("|      ^^^^*^^^^      |");
+		System.out.println("|         ***         |");
+		System.out.println("|        *****        |");
+		System.out.println("|       *******       |");
+		System.out.println("|       !-O-O-!       |");
+		System.out.println("|       |  O  |       |");
+		System.out.println("|       |  U  |       |");
+		System.out.println("|       $$$$$$$       |");
+		System.out.println("|   |___$$$$$$$___|   |");
+		System.out.println("|       $$$$$$$       |");
+		//System.out.println("|                     |");
+		System.out.println("+---------------------+");
+
+	}
+}
