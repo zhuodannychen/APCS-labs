f1bb8b94c7d39328d5ea1aaeb5333e929e1babdd
Danny Chen <37634165+zhuodannychen@users.noreply.github.com>
2018-12-03 11:15:41 -0600

    Update SportsReport.java

--- unit_7/SportsReportLab/SportsReport.java
+++ unit_7/SportsReportLab/SportsReport.java
@@ -9,133 +9,133 @@
  
 public class SportsReport
 {
-  private String winner;
-  private String loser;
-  private int winningScore;
-  private int losingScore;
-  public static final String BEAT = " beat ";
-  public static final String BY = " by ";
-  public static final String OF = " of ";
-  public static final String TO = " to ";
+	private String winner;
+	private String loser;
+	private int winningScore;
+    private int losingScore;
+  	public static final String BEAT = " beat ";
+  	public static final String BY = " by ";
+  	public static final String OF = " of ";
+  	public static final String TO = " to ";
 
-  /** Constructs a SportsReport using the information in score
-   *  @param score contains winner, loser, and scores in the format:
-   *  <winner> beat <loser> by a score of <winning score> to <losing score>
-   */
-  public SportsReport(String score)
-  {
-    extractInformation(score);
-  }
+  	/** Constructs a SportsReport using the information in score
+   	*  @param score contains winner, loser, and scores in the format:
+   	*  <winner> beat <loser> by a score of <winning score> to <losing score>
+   	*/
+	public SportsReport(String score)
+  	{
+    	extractInformation(score);
+  	}
 
-  /** Changes the sports scoring information
-   *  @param score contains winner, loser, and scores in the format:
-   *  <winner> beat <loser> by a score of <winning score> to <losing score>
-   */
-  public void changeScoringInfo(String score)
-  {
-    extractInformation(score);
-  }
+  	/** Changes the sports scoring information
+   	*  @param score contains winner, loser, and scores in the format:
+   	*  <winner> beat <loser> by a score of <winning score> to <losing score>
+   	*/
+  	public void changeScoringInfo(String score)
+  	{
+    	extractInformation(score);
+  	}
 
-  //Accessor methods
+  	//Accessor methods
+  	
+  	/**Gets the winniner's name
+  	*@return the winner
+  	*/
+  	public String getWinner()
+  	{
+		return winner;
+  	}
+  	/**Gets the loser's name
+   	*@return the loser team
+   	*/
+  	public String getLoser()
+  	{
+    	return loser;
+  	}
+  	/**Gets the winningScore
+   	*@return the winning score
+   	*/
+  	public int getWinningScore()
+  	{
+    	return winningScore;
+  	}
+  	/**Gets the losingScore
+   	*@return the losing score
+   	*/
+  	public int getLosingScore()
+  	{
+    	return losingScore;
+  	}
+
+  	/**Gets the score difference for the winning and losing scores
+   	*@return the score difference
+   	*/
+  	public int getScoreDifference()
+  	{
+    	return winningScore - losingScore;
+  	}
+
+  	@Override
+  	public String toString()
+  	{
+  		//formats the string
+  		String finall = String.format("%-25s%4d, %-25s%4d", winner, winningScore, loser, losingScore);
+    	return finall;
+  	}
   
-  /**Gets the winniner's name
-  *@return the winner
-  */
-  public String getWinner()
-  {
-    return winner;
-  }
-  /**Gets the loser's name
-   *@return the loser team
-   */
-  public String getLoser()
-  {
-    return loser;
-  }
-  /**Gets the winningScore
-   *@return the winning score
-   */
-  public int getWinningScore()
-  {
-    return winningScore;
-  }
-  /**Gets the losingScore
-   *@return the losing score
-   */
-  public int getLosingScore()
-  {
-    return losingScore;
-  }
+  	/**This private method takes all the important information from a string called scores,
+   	*it should update winer, loser, winningScore, and losingScore.
+   	*@param score contains winner, loser, and scores in the format:
+   	*<winner> beat <loser> by a score of <winning score> to <losing score>
+   	*/
+  	private void extractInformation(String score)
+  	{
+    	int indexOfBeat = -1;
+    	int indexOfBy = -1;
+  		int indexOfOff = -1;
+   		int indexOfTo = -1;
 
-  /**Gets the score difference for the winning and losing scores
-   *@return the score difference
-   */
-  public int getScoreDifference()
-  {
-    return winningScore - losingScore;
-  }
-
-  @Override
-  public String toString()
-  {
-  	//formats the string
-  	String finall = String.format("%-25s%4d, %-25s%4d", winner, winningScore, loser, losingScore);
-    return finall;
-  }
-  
-  /**This private method takes all the important information from a string called scores,
-   *it should update winer, loser, winningScore, and losingScore.
-   *@param score contains winner, loser, and scores in the format:
-   *<winner> beat <loser> by a score of <winning score> to <losing score>
-   */
-  private void extractInformation(String score)
-  {
-    int indexOfBeat = -1;
-    int indexOfBy = -1;
-  	int indexOfOff = -1;
-   	int indexOfTo = -1;
-
-    for(int i = 0; i < score.length() - 1; i++)
-    {
-      if(score.substring(i, i + 6).equals(BEAT))
-      {
-        indexOfBeat = i;
-      	winner = score.substring(0, i + 1).trim();
-        break;
-      }
-    }
+    	for(int i = 0; i < score.length() - 1; i++)
+    	{
+      		if(score.substring(i, i + 6).equals(BEAT))
+      		{
+        		indexOfBeat = i;
+      			winner = score.substring(0, i + 1).trim();
+        		break;
+      		}
+    	}
     
-    for(int j = indexOfBeat; j < score.length() - 1; j++)
-    {
-      if(score.substring(j, j + 4).equals(BY))
-      {
-       	indexOfBy = j;
-       	loser = score.substring(indexOfBeat + 6, indexOfBy);
-       	break;
-      }     
-   	}
+    	for(int j = indexOfBeat; j < score.length() - 1; j++)
+    	{
+      		if(score.substring(j, j + 4).equals(BY))
+      		{
+       			indexOfBy = j;
+       			loser = score.substring(indexOfBeat + 6, indexOfBy);
+       			break;
+      		}     
+   		}
 
-   for(int i = indexOfBy; i < score.length() - 1; i++)
-   {
-      if(score.substring(i, i + 4).equals(OF))
-      {
-        indexOfOff = i;
-       	break;
-      }     
-   }
+   		for(int i = indexOfBy; i < score.length() - 1; i++)
+   		{
+      		if(score.substring(i, i + 4).equals(OF))
+      		{
+        		indexOfOff = i;
+       			break;
+      		}     
+   		}
 
-   for(int j = indexOfOff; j < score.length() - 1; j++)
-   {
-      if(score.substring(j, j + 4).equals(TO))
-      {
-      	indexOfTo = j;
-       	String stringWinScore = score.substring(indexOfOff + 3, indexOfTo).trim();
-       	winningScore = Integer.parseInt(stringWinScore);
-       	break;
-      }     
-   }   
+   		for(int j = indexOfOff; j < score.length() - 1; j++)
+   		{
+      		if(score.substring(j, j + 4).equals(TO))
+      		{
+      			indexOfTo = j;
+       			String stringWinScore = score.substring(indexOfOff + 3, indexOfTo).trim();
+       			winningScore = Integer.parseInt(stringWinScore);
+       			break;
+      		}     
+   		}   
   
-   String stringloseScore = score.substring(indexOfTo + 3, score.length()).trim();
-   losingScore = Integer.parseInt(stringloseScore);
+   		String stringloseScore = score.substring(indexOfTo + 3, score.length()).trim();
+   		losingScore = Integer.parseInt(stringloseScore);
   }
 }
